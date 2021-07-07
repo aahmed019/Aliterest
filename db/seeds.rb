67d4c3f363bb119dde31bafcc14f925cee81e042
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-06 21:10:30 -0400

    changed seeding for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,5 @@ Location.destroy_all
 
 jack = User.create({email: 'jack@fakeemail.com', f_name: 'Jack', l_name: 'Liu',  password: 'test123'})
 sam = User.create({email: 'sam@fakeemail.com', f_name: 'Sam', l_name: 'Sacco',  password: 'test123'})
-justin = User.create({email: 'justin@fakeemail.com', f_name: 'Justin', l_name: 'Meyer',  password: 'test123'})
-
 bear = Location.create(title: 'Demo Title', description: 'this is a demo description', price: 100, latitude: '41.3129', longitude: '74.0063', host_id: 2)
-gateway = Location.create(title: 'Demo Title', description: 'this is a demo description', price: 95, latitude: '42.3658', longitude: '71.0263', host_id: 1)
+gateway = Location.create(title: 'Demo 2 Title', description: 'This is the second description', price: 95, latitude: '42.3658', longitude: '71.0263', host_id: 1)
